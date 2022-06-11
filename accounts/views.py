86e0159a9fe478dcaf197a78d3924d9330ce133c
retech <itsregalo047@gmail.com>
2022-06-11 12:21:12 +0300

    health index

--- accounts/views.py
+++ accounts/views.py
@@ -39,6 +39,7 @@ def LogInView(request, *args, **kwargs):
         if form.is_valid():
             username = form.cleaned_data.get('username')
             password = form.cleaned_data.get('password')
+            
             user = authenticate(username=username, password=password)
             if user is not None:
                 if user.is_active:
